d0a3859f7cc1a40a6ae4de0fbdc439cd700a65ba
ale <aledeum.saf@gmail.com>
2024-10-11 20:21:51 -0300

    testing new version

---
+++ cadigo1.h
@@ -0,0 +1,228 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef long double val;
+#define VAL_SIZE_BITS (sizeof(val)*8)
+#define VAL_FMT "%LF"
+
+
+val power(val base, val exponent) {
+    val result = 1;
+    for (val i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+#define MILIMETER 1.0L
+
+#define km  * (MILIMETER * 1000000)
+#define hm  * (MILIMETER * 100000)
+#define dam * (MILIMETER * 10000)
+#define m   * (MILIMETER * 1000)
+#define dm  * (MILIMETER * 100)
+#define cm  * (MILIMETER * 10)
+#define mm  * (MILIMETER * 1)
+#define um  * (MILIMETER / 1000)
+#define mic * (MILIMETER / 1000)
+#define nm  * (MILIMETER / 1000000)
+#define pm  * (MILIMETER / 1000000000)
+
+
+val static inline as_km (val v) { return v / (MILIMETER * 1000000); }
+val static inline as_hm (val v) { return v / (MILIMETER * 100000); }
+val static inline as_dam(val v) { return v / (MILIMETER * 10000); }
+val static inline as_m  (val v) { return v / (MILIMETER * 1000); }
+val static inline as_dm (val v) { return v / (MILIMETER * 100); }
+val static inline as_cm (val v) { return v / (MILIMETER * 10); }
+val static inline as_mm (val v) { return v / (MILIMETER * 1); }
+val static inline as_um (val v) { return v / (MILIMETER / 1000); }
+val static inline as_mic(val v) { return v / (MILIMETER / 1000); }
+val static inline as_nm (val v) { return v / (MILIMETER / 1000000); }
+val static inline as_pm (val v) { return v / (MILIMETER / 1000000000); }
+
+
+typedef struct {
+    val x;
+    val y;
+} Vec2;
+
+Vec2 v2(val x, val y) { return (Vec2){x, y}; }
+
+typedef struct {
+    val x;
+    val y;
+    val z;
+} Vec3;
+
+Vec3 v3(val x, val y, val z) { return (Vec3){x, y, z}; }
+
+Vec3 vec3_add(Vec3 a, Vec3 b) {
+    Vec3 ret = {a.x + b.x, a.y + b.y, a.z + b.z};
+    return ret;
+}
+Vec3 vec3_substract(Vec3 a, Vec3 b) {
+    return v3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+Vec3 vec3_rotate_y(val rotation, Vec3 p) {
+    double angle = atan2(p.z, p.x) + rotation;
+    double mag = sqrt(p.x*p.x + p.z*p.z);
+    return v3(cos(angle)*mag, p.y, sin(angle)*mag);
+}
+
+
+typedef struct {
+    size_t count;
+    Vec2* items;
+} Vec2Array;
+
+#define vecs2(...) \
+    new__vec2__array(sizeof((Vec2[]){__VA_ARGS__}), (Vec2[]){__VA_ARGS__}) 
+
+Vec2Array new__vec2__array(size_t size, Vec3* ptr){
+    return (Vec2Array) {
+        .count = size / sizeof(Vec2),
+        .items = (Vec2*)memcpy(malloc(size), ptr, size)
+    };
+}
+
+typedef struct {
+    size_t count;
+    Vec3* items;
+} Vec3Array;
+
+#define vecs3(...) \
+    new__vec3__array(sizeof((Vec3[]){__VA_ARGS__}), (Vec3[]){__VA_ARGS__})
+
+Vec3Array new__vec3__array(size_t size, Vec3* ptr){
+    return (Vec3Array) {
+        .count = size / sizeof(Vec3),
+        .items = (Vec3*)memcpy(malloc(size), ptr, size)
+    };
+}
+
+
+typedef struct {
+    size_t count;
+    val* items;
+} ValueArray;
+
+#define vals(...) \
+    new__value_array(sizeof((val[]){__VA_ARGS__}), (val[]){__VA_ARGS__}) 
+
+ValueArray new__value_array(size_t size, val* ptr){
+    return (ValueArray) {
+        .count = size / sizeof(val),
+        .items = (val*)memcpy(malloc(size), ptr, size)
+    };
+}
+
+
+typedef struct {
+    size_t count;
+    size_t* items;
+} Face;
+
+#define face(...) \
+    new__face(sizeof((size_t[]){__VA_ARGS__}), (size_t[]){__VA_ARGS__}) 
+
+Face new__face(size_t size, size_t* ptr){
+    return (Face) {
+        .count = size / sizeof(size_t),
+        .items = (size_t*)memcpy(malloc(size), ptr, size)
+    };
+}
+
+typedef struct {
+    size_t count;
+    Face* items;
+} Faces;
+
+#define faces(...) \
+    new__faces(sizeof((Face[]){__VA_ARGS__}), (Face[]){__VA_ARGS__}) 
+
+Faces new__faces(size_t size, Face* ptr){
+    return (Faces) {
+        .count = size / sizeof(Face),
+        .items = (Face*)memcpy(malloc(size), ptr, size)
+    };
+}
+
+// --- Swizzling ---
+// -- Simple (useful for macros like map)
+#define x(vec) (vec.x)
+#define y(vec) (vec.y)
+#define z(vec) (vec.z)
+
+// -- Double 
+#define xx(vec) (vec2(vec.x, vec.x))
+#define xy(vec) (vec2(vec.x, vec.y))
+#define yx(vec) (vec2(vec.y, vec.x))
+#define yy(vec) (vec2(vec.y, vec.y))
+#define xz(vec) (vec2(vec.x, vec.z))
+#define yz(vec) (vec2(vec.y, vec.z))
+#define zx(vec) (vec2(vec.z, vec.x))
+#define zy(vec) (vec2(vec.z, vec.y))
+#define zz(vec) (vec2(vec.z, vec.z))
+
+// -- Triple
+#define xxx(vec) (vec3(vec.x, vec.x, vec.x))
+#define xxy(vec) (vec3(vec.x, vec.x, vec.y))
+#define xxz(vec) (vec3(vec.x, vec.x, vec.z))
+#define xyx(vec) (vec3(vec.x, vec.y, vec.x))
+#define xyy(vec) (vec3(vec.x, vec.y, vec.y))
+#define xyz(vec) (vec3(vec.x, vec.y, vec.z))
+#define xzx(vec) (vec3(vec.x, vec.z, vec.x))
+#define xzy(vec) (vec3(vec.x, vec.z, vec.y))
+#define xzz(vec) (vec3(vec.x, vec.z, vec.z))
+#define yxx(vec) (vec3(vec.y, vec.x, vec.x))
+#define yxy(vec) (vec3(vec.y, vec.x, vec.y))
+#define yxz(vec) (vec3(vec.y, vec.x, vec.z))
+#define yyx(vec) (vec3(vec.y, vec.y, vec.x))
+#define yyy(vec) (vec3(vec.y, vec.y, vec.y))
+#define yyz(vec) (vec3(vec.y, vec.y, vec.z))
+#define yzx(vec) (vec3(vec.y, vec.z, vec.x))
+#define yzy(vec) (vec3(vec.y, vec.z, vec.y))
+#define yzz(vec) (vec3(vec.y, vec.z, vec.z))
+#define zxx(vec) (vec3(vec.z, vec.x, vec.x))
+#define zxy(vec) (vec3(vec.z, vec.x, vec.y))
+#define zxz(vec) (vec3(vec.z, vec.x, vec.z))
+#define zyx(vec) (vec3(vec.z, vec.y, vec.x))
+#define zyy(vec) (vec3(vec.z, vec.y, vec.y))
+#define zyz(vec) (vec3(vec.z, vec.y, vec.z))
+#define zzx(vec) (vec3(vec.z, vec.z, vec.x))
+#define zzy(vec) (vec3(vec.z, vec.z, vec.y))
+#define zzz(vec) (vec3(vec.z, vec.z, vec.z))
+
+
+typedef struct {
+    Vec3Array points;
+    Faces faces;  
+} Object3D;
+
+
+Object3D obj_cube(val size) {
+    val a = size/2;
+    Object3D c = {
+        .points = vecs3(
+            v3(-a, -a, -a),
+            v3(-a, -a,  a),
+            v3(-a,  a,  a),
+            v3( a,  a,  a),
+
+            v3(-a,  a, -a),
+            v3( a, -a,  a),
+            v3( a,  a, -a),
+            v3( a, -a, -a),
+        )
+    };
+    return c;
+}
+
+
+
+
---
+++ cadigo1_test.c
@@ -0,0 +1,145 @@
+#include <stdbool.h>
+#include <SDL2/SDL_render.h>
+#include <time.h>
+#include "cadigo1.h"
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 600
+#define EPS 1
+
+typedef struct {
+    Vec3 position;
+    Vec3 rotation;
+    val zoom;
+} Camera;
+
+
+static Vec2 project_3d_2d(Vec3 v, Camera* camera) {
+    Vec3 v1 = v;
+    v1 = vec3_substract(v1, camera->position);
+    v1 = vec3_rotate_y (camera->rotation.y, v1);
+    v1.z *= camera->zoom;
+
+    return v2(v1.x/v1.z, v1.y/v1.z);
+}
+
+static Vec2 project_2d_scr(Vec2 v) {
+    return v2((v.x + 1)/2*WINDOW_WIDTH/8 + WINDOW_WIDTH/2., (1 - (v.y + 1)/2)*WINDOW_WIDTH/8 + WINDOW_WIDTH/2.);
+}
+
+#define NSPF 100000000 / 2
+
+#define SENSITIVITY 500000000. / NSPF
+#define SENSITIVITY_ROT 0.01
+
+int main(void) {
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    int i;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+
+    Camera cam;
+    cam.position = v3(0, -10, -100);
+    cam.rotation = v3(0, 0, 0);
+    cam.zoom = 1;
+
+    bool shifting        = false;
+    bool moving_forward  = false;
+    bool moving_backward = false;
+    bool moving_left     = false;
+    bool moving_right    = false;
+    bool moving_up       = false;
+    bool moving_down     = false;
+
+    bool loop = true; 
+
+    while (loop) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT){
+                loop = false;
+                break;
+            }
+
+            if (event.type == SDL_KEYUP) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_LSHIFT:
+                    case SDLK_RSHIFT: shifting   = false; break;
+                    case SDLK_k: moving_up       = false; break;
+                    case SDLK_j: moving_down     = false; break;
+                    case SDLK_l: moving_left     = false; break;
+                    case SDLK_h: moving_right    = false; break;
+                    case SDLK_i: moving_forward  = false; break;
+                    case SDLK_m: moving_backward = false; break;
+                    default:
+                        printf("Key pressed: %s\n", SDL_GetKeyName(event.key.keysym.sym));
+                        break;
+                }
+            }
+
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_LSHIFT:
+                    case SDLK_RSHIFT: shifting   = true; break;
+                    case SDLK_k: moving_up       = true; break;
+                    case SDLK_j: moving_down     = true; break;
+                    case SDLK_l: moving_left     = true; break;
+                    case SDLK_h: moving_right    = true; break;
+                    case SDLK_i: moving_forward  = true; break;
+                    case SDLK_m: moving_backward = true; break;
+                    default:
+                        printf("Key pressed: %s\n", SDL_GetKeyName(event.key.keysym.sym));
+                        break;
+                }
+            }
+        }
+
+        if (moving_up)       cam.position.y -= SENSITIVITY;
+        if (moving_down)     cam.position.y += SENSITIVITY;
+        if (moving_left)     cam.position.x += SENSITIVITY;
+        if (moving_right)    cam.position.x -= SENSITIVITY;
+        if (moving_forward)  cam.position.z += SENSITIVITY;
+        if (moving_backward) cam.position.z -= SENSITIVITY;
+
+        Object3D c = obj_cube(10);
+        for (size_t i = 0; i < c.points.count; ++i) {
+            Vec3 p = c.points.items[i];
+            //p = vec3_add(v3(x, 10, 100), p);
+            //printf("point %zu:"VAL_FMT", "VAL_FMT", "VAL_FMT"\n", i, as_mm(p.x), as_mm(p.y), as_mm(p.z));
+
+            Vec2 p2 = project_3d_2d(p, &cam);
+            p2.x = p2.x * WINDOW_WIDTH + WINDOW_WIDTH / 2.;
+            p2.y = p2.y * WINDOW_WIDTH + WINDOW_WIDTH / 2.;
+
+
+            //printf("point2d %zu: "VAL_FMT", "VAL_FMT"\n", i, p2.x, p2.y);
+            SDL_RenderDrawPoint(renderer, p2.x, p2.y);
+        }
+
+        SDL_RenderPresent(renderer);
+
+        struct timespec req;
+        req.tv_sec = 0;
+        req.tv_nsec = NSPF;
+        nanosleep(&req, NULL);
+    }
+
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
+// int main() {
+//     Object3D c = obj_cube(1 cm);
+//     for (size_t i = 0; i < c.points.count; ++i) {
+//         Vec3 p = c.points.items[i];
+//         printf("point %zu %ld, %ld, %ld\n", i, as_mm(p.x), as_mm(p.y), as_mm(p.z));
+//     }
+// }
